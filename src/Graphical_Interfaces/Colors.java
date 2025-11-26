735ec38d43d31e23cb8e7d084c35a9a124f104f6
Érick <98136250+Rick512P@users.noreply.github.com>
2025-11-26 16:03:52 -0300

    Classe Colors
    Gerada classe para definição de cores padrões a serem usadas

---
+++ src/Graphical_Interfaces/Colors.java
@@ -0,0 +1,16 @@
+package Graphical_Interfaces;
+
+import java.awt.*;
+
+public final class Colors {
+    private Colors() {} // Previni instanciacao
+
+    public static final Color GENERAL_BACKGROUND = new Color(177, 134, 69);
+    public static final Color GENERAL_BUTTON = new Color(161, 235, 246);
+    public static final Color OPTION_BUTTON = new Color(57, 57, 57);
+    public static final Color END_GAME_BUTTON = new Color(153, 0, 0);
+    public static final Color TITLE_TEXT = new Color(153, 0, 0);
+    public static final Color TEXT_BUTTON = new Color(0, 0, 0);
+    public static final Color TEXT_OPTION_BUTTON = new Color(232,232,232);
+    public static final Color TEXT_END_GAME_BUTTON = new Color(232, 232, 232);
+}
