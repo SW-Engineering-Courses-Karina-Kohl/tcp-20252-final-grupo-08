184a5024569d7aabd665e5ba2569217d2d1c885e
Fernando Tedesco <fernandotedesco123@gmail.com>
2025-11-29 20:13:28 -0300

    Refatoração MonsterCard e SpellCard

--- src/Domain/GameAction.java
+++ src/Domain/GameAction.java
@@ -1,10 +1,6 @@
 package Domain;
 
-// Define o que um botão de ação faz.
-public abstract class GameAction { 
-
-    public GameAction() { } 
-    
-    public abstract String getLabel(); 
-    public abstract void execute();    
+public interface GameAction { 
+    String getLabel(); 
+    void execute();    
 }
--- src/Domain/SpellCard.java
+++ src/Domain/SpellCard.java
@@ -1,49 +1,13 @@
 package Domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class SpellCard implements Card {
-    
-    private final String name;
-    private final String description;
-    private final String imagePath;
-    private final int cost;
+public class SpellCard extends AbstractCard {
 
     public SpellCard(String name, String description, String imagePath, int cost) {
-        this.name = name;
-        this.description = description; 
-        this.imagePath = imagePath;
-        this.cost = cost;
+        super(name, description, imagePath, cost);
     }
 
     @Override
     public CardType getType() {
         return CardType.SPELL;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public String getDescription() {
-        return description;
-    }
-
-    @Override
-    public String getImagePath() {
-        return imagePath;
-    }
-
-    @Override
-    public int getCost() {
-        return cost;
-    }
-
-    @Override
-    public List<GameAction> getAvailableActions() {
-        return new ArrayList<>();
     }
 }
