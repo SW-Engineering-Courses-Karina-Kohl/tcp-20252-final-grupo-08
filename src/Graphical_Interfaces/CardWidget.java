8fc4f550024a71a1ab4f8742d35a205d0bcc1636
Fernando Tedesco <fernandotedesco123@gmail.com>
2025-11-29 17:02:35 -0300

    Implementado sistema de imagem das cartas

--- src/Graphical_Interfaces/CardWidget.java
+++ src/Graphical_Interfaces/CardWidget.java
@@ -4,13 +4,15 @@ import Domain.Card;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.net.URL;
 import javax.swing.*;
 
 public class CardWidget extends JPanel {
     private final Card cardData;
     private boolean isHovered = false;
+    private Image cardImage;
+    private JLabel nameLabel;
 
-    
     public interface OnCardClickListener {
         void onCardClicked(Card card);
     }
@@ -21,9 +23,17 @@ public class CardWidget extends JPanel {
         setBackground(Colors.GENERAL_BUTTON);
         setBorder(BorderFactory.createLineBorder(Colors.TEXT_BUTTON, 2));
         setLayout(new BorderLayout()); 
+
+        URL imageUrl = getClass().getResource("/resources/placeholder.png"); 
+    
+        if (imageUrl != null) {
+            this.cardImage = new ImageIcon(imageUrl).getImage();
+        } else {
+            System.err.println("Imagem placeholder.png não encontrada!");
+        }
         
         //Texto em html para não ficar cortado
-        JLabel nameLabel = new JLabel("<html><center>" + card.getName() + "</center></html>");
+        nameLabel = new JLabel("<html><center>" + card.getName() + "</center></html>");
         nameLabel.setForeground(Colors.TEXT_BUTTON);
         nameLabel.setFont(new Font("Serif", Font.BOLD, 14));
         nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -69,6 +79,17 @@ public class CardWidget extends JPanel {
         }
         g2d.fillRect(0, 0, getWidth(), getHeight());
 
+        if (cardImage != null) {
+            int imgW = (int) (getWidth() * 0.6); 
+            int imgH = (int) (getHeight() * 0.5); 
 
+            int titleHeight = nameLabel.getHeight();
+            int availableHeight = getHeight() - titleHeight;
+            
+            int x = (getWidth() - imgW) / 2;
+            int y = titleHeight + (availableHeight - imgH) / 2;
+
+            g2d.drawImage(cardImage, x, y, imgW, imgH, this);
+        }
     }
 }
